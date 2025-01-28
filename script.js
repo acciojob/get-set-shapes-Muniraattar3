a27c6a48e2f96cf16b6cca3e096de95f34c9e50b
Munira Yusuf Ali Attar <124035031+Muniraattar3@users.noreply.github.com>
2025-01-28 22:51:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width; // Private property for width
+    this._height = height; // Private property for height
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width; // Getter for width
+  }
+
+  get height() {
+    return this._height; // Getter for height
+  }
+
+  getArea() {
+    return this._width * this._height; // Calculate and return the area
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // Call the parent constructor with width and height as side
+  }
+
+  getPerimeter() {
+    return 4 * this.width; // Calculate and return the perimeter
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
